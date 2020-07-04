a4c83420bc15fe36ff31a69626e2a7b26ab25342
liduen <katrina.truebenbach@gmail.com>
2020-07-04 11:00:51 -0400

    add visualization option

--- pages/home_page.py
+++ pages/home_page.py
@@ -15,9 +15,10 @@ def write():
     st.header('Select the type of recommendation you want on the left menu')
     st.write("""
     Options:
-    - Top Rated Movies: apply filters to find the top rated movies in your desired attributes
-    - Movie Based Recommendations: enter a movie you have previously enjoyed to view similar movies
-    - Personalized Recommendations: enter your user ID to find your personalized top movies + apply filters
-    - Add Profile: manually enter movie ratings if you are not in our system, thereby creating a profile to enable personalized recommendations
+    - **Top Movie Visualizations**: view visualizations of the movies in our catalog to understand what makes a great movie.
+    - **Top Rated Movies**: apply filters to find the top rated movies in your desired attributes
+    - **Movie Based Recommendations**: enter a movie you have previously enjoyed to view similar movies
+    - **Personalized Recommendations**: enter your user ID to find your personalized top movies + apply filters
+    - **Add Profile**: manually enter movie ratings if you are not in our system, thereby creating a profile to enable personalized recommendations
     """)
 
