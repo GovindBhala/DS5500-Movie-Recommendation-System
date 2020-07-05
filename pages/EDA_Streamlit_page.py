bd9eae433a16b46c5a38d90526d039c34902e8e8
Shwean <anandshweta0902@gmail.com>
2020-07-05 11:17:36 -0400

    changed title names

--- pages/EDA_Streamlit_page.py
+++ pages/EDA_Streamlit_page.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[19]:
+# In[1]:
 
 
 import pandas as pd
@@ -21,137 +21,88 @@ from PIL import Image
 # # Change the directory name
 # - image rec folder is also uploaded
 
-# In[24]:
+# In[2]:
 
 
-def write():
-    #directory_name ='/Users/shwetaanand/DEVHOME/Images_rec'
-    directory_name = 'pages/Images_rec'
-    
-    st.markdown("<h1 style='text-align: center; color: red;'>Exploratory Data Analysis</h1>", unsafe_allow_html=True)
+directory_name ='/Users/shwetaanand/DEVHOME/Images_rec'
 
-    st.markdown("<h1 style='text-align: center; color: blue;'>Select Check Box - to display EDA</h1>", unsafe_allow_html=True)
 
-    st.write("\n")
-    st.write("\n")
-    
+# In[3]:
 
-    if st.checkbox('Genres Frequency'):
-        image_file = 'Frequency_Genres'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
-    
-    if st.checkbox('Top 10 Genre Combinations'):
-        image_file = 'Top 10 Genre Combinations'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
 
-    if st.checkbox('Top 10 Actors/Actresses'):
-        image_file = 'Top 10 Actors or Actresses'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
+st.markdown("<h1 style='text-align: center; color: red;'>Exploratory Data Analysis</h1>", unsafe_allow_html=True)
 
-    if st.checkbox('Top 10 Directors'):
-        image_file = 'Top 10 Directors'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image,  use_column_width=True)
+st.markdown("<h1 style='text-align: center; color: blue;'>Select Check Box - to display EDA</h1>", unsafe_allow_html=True)
 
-    if st.checkbox('Top 10 Filming Countries'):
-        image_file = 'Top 10 Filming Countries'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
 
-    if st.checkbox('Top 10 (Relevant greater than 50%) Genome Tags'):
-        image_file = 'Top 10 (Relevant greater than 50%) Genome Tags'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
+# In[6]:
+
+
+if st.checkbox('Movie count by Genre'):
+    image_file = 'Movie count by Genre'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)
         
-    if st.checkbox('Genres by Average Rating'):
-        image_file = 'Genres by Average Rating'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
+if st.checkbox('Most Common Genre Combinations'):
+    image_file = 'Top 10 Genre Combinations'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)
+        
 
-    if st.checkbox('Top 10 Highly Rated Actors -  Minimum 100 Ratings'):
-        image_file = 'Top 10 Highly Rated Actors - Minimum 100 Ratings'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
+if st.checkbox('Most Common Actors'):
+    image_file = 'Top 10 Actors or Actresses'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)
+        
+            
+if st.checkbox('Most Rated Actors'):
+    image_file = 'Top 10 Most Rated Actors'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)
+            
 
-    if st.checkbox('Top 10 Highly Rated Directors - Minimum 100 Ratings'):
-        image_file = 'Top 10 Highly Rated Directors - Minimum 100 Ratings'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
+if st.checkbox('Most Common Directors'):
+    image_file = 'Top 10 Directors'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)
+            
+if st.checkbox('Most Rated Directors'):
+    image_file = 'Top 10 Most Rated Directors'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)
 
-    if st.checkbox('Top 10 Highly Rated Movies - Minimum 100 Ratings'):
-        image_file = 'Top 10 Highly Rated Movies - Minimum 100 Ratings'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
+if st.checkbox('Number of Movies by Filming Country'):
+    image_file = 'Top 10 Filming Countries'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)
 
-    if st.checkbox('Top 10 Highly Rated Movies - Weighted Average'):
-        image_file = 'Top 10 Highly Rated Movies - Weighted Average'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
+    
+if st.checkbox('Most Rated Movies'):
+    image_file = 'Top 10 Most Rated Movies'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)
 
-    if st.checkbox('Distribution of Ratings'):
-        image_file = 'Distribution of Ratings'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
-
-    if st.checkbox('Number of Ratings (millions)'):
-        image_file = 'Number of Ratings (millions)'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
-
-    if st.checkbox('Distribution of Number of Tags per Movie'):
-        image_file = 'Distribution of Number of Tags per Movie'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
-
-    if st.checkbox('Distribution of Number Ratings by Movie- Many (Unpopular Movies)'):
-        image_file = 'Distribution of Number Ratings by Movie- Many (Unpopular Movies)'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
-
-    if st.checkbox('Distribution of Tag Relevance Scores'):
-        image_file = 'Distribution of Tag Relevance Scores'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
-
-    if st.checkbox('Release Year Frequency Counts'):
-        image_file = 'Release Year Frequency Counts'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
+if st.checkbox('Number Of Movie Released Per Year'):
+    image_file = 'Number Of Movie Released Per Year'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)
+                       
 
