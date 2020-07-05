a3b49d3df16007481bd1d066052825e066bd9267
shwean <47954595+shwean@users.noreply.github.com>
2020-07-05 13:40:07 -0400

    Add files via upload

--- pages/EDA_Streamlit_page.py
+++ pages/EDA_Streamlit_page.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[7]:
 
 
 import pandas as pd
@@ -21,88 +21,90 @@ from PIL import Image
 # # Change the directory name
 # - image rec folder is also uploaded
 
-# In[2]:
+# In[8]:
 
 
-#directory_name ='/Users/shwetaanand/DEVHOME/Images_rec'
+directory_name ='/Users/shwetaanand/DEVHOME/Images_rec'
 
 
 # In[6]:
 
 
-def write():
+st.markdown("<h1 style='text-align: center; color: red;'>Exploratory Data Analysis</h1>", unsafe_allow_html=True)
+st.text("")
+st.text("")
+st.markdown("<h1 style='text-align: center; color: blue;'>Select Check Box - to display EDA</h1>", unsafe_allow_html=True)
+st.text("")
+st.text("")
+
+if st.checkbox('Movie count by Genre'):
+    image_file = 'Movie count by Genre'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)
+        
+if st.checkbox('Top 10 Genre Combinations'):
+    image_file = 'Top 10 Genre Combinations'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)
+        
+
+if st.checkbox('Top 10 Most Common Actors'):
+    image_file = 'Top 10 Actors or Actresses'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)
+
+if st.checkbox('Top 10 Most Common Directors'):
+    image_file = 'Top 10 Directors'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)
+
+if st.checkbox('Top 10 Filming Countries'):
+    image_file = 'Top 10 Filming Countries'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)            
+
+
+if st.checkbox('Top 10 Highly Rated Actors*'):
+    image_file = 'Top 10 Most Rated Actors'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)
+            
+
+if st.checkbox('Top 10 Highly Rated Directors*'):
+    image_file = 'Top 10 Most Rated Directors'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)
     
-    directory_name = 'pages/Images_rec'
+if st.checkbox('Top 10 Highly Rated Movies*'):
+    image_file = 'Top 10 Most Rated Movies'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)
 
-    st.markdown("<h1 style='text-align: center; color: red;'>Exploratory Data Analysis</h1>", unsafe_allow_html=True)
-
-    st.markdown("<h1 style='text-align: center; color: blue;'>Select Check Box - to display EDA</h1>", unsafe_allow_html=True)
-
-
-    if st.checkbox('Movie count by Genre'):
-        image_file = 'Movie count by Genre'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
-
-    if st.checkbox('Most Common Genre Combinations'):
-        image_file = 'Top 10 Genre Combinations'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
-
-
-    if st.checkbox('Most Common Actors'):
-        image_file = 'Top 10 Actors or Actresses'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
-
-
-    if st.checkbox('Most Rated Actors'):
-        image_file = 'Top 10 Most Rated Actors'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
-
-
-    if st.checkbox('Most Common Directors'):
-        image_file = 'Top 10 Directors'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
-
-    if st.checkbox('Most Rated Directors'):
-        image_file = 'Top 10 Most Rated Directors'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
-
-    if st.checkbox('Number of Movies by Filming Country'):
-        image_file = 'Top 10 Filming Countries'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
-
-
-    if st.checkbox('Most Rated Movies'):
-        image_file = 'Top 10 Most Rated Movies'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
-
-    if st.checkbox('Number Of Movie Released Per Year'):
-        image_file = 'Number Of Movie Released Per Year'
-        suffix='.png'
-        img_file =os.path.join(directory_name, image_file + suffix)
-        image = Image.open(img_file)
-        st.image(image, use_column_width=True)
+if st.checkbox('Number Of Movie Released Per Year'):
+    image_file = 'Number Of Movie Released Per Year'
+    suffix='.png'
+    img_file =os.path.join(directory_name, image_file + suffix)
+    image = Image.open(img_file)
+    st.image(image, use_column_width=True)                      
+st.text("")
+st.text("")
+st.text('*Weighted Average Rating between average ratings and number of ratings ')
+st.text("")
+st.text("")
 
